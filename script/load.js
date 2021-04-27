7738737c10209a165b7a81c67c4f983f8d163093
Izzatulloh <izzatulloh5566@gmail.com>
2021-04-27 14:35:41 +0500

    test

--- script/load.js
+++ script/load.js
@@ -1,6 +1,8 @@
+
 window.addEventListener('load',setTimeout(() => {
     let preloader = document.getElementById('priloader')
     let body = document.querySelector('body')
+    
     preloader.style.display = 'none'
     body.style.overflow = 'visible'
 }, 2000))
--- script/scroll.js
+++ script/scroll.js
@@ -4,7 +4,7 @@ window.addEventListener('scroll', function() {
     
     if(pageYOffset> 100){
       
-            nav.style.background = 'rgba(255, 255, 255,0.7)'
+            nav.style.background = '#fff'
             nav.style.transition= '0.5s'
     }
     else if(pageYOffset< 100){
