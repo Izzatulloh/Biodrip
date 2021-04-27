b420813fe22d763dc5af261c9e9c1b71367d9431
Izzatulloh <izzatulloh5566@gmail.com>
2021-04-27 12:54:53 +0500

    test

--- script/menu.js
+++ script/menu.js
@@ -1,3 +1,13 @@
+window.addEventListener('onload',setTimeout(() => {
+    let preloader = document.getElementById('priloader')
+    let body = document.querySelector('body')
+    preloader.style.display = 'none'
+    body.style.overflow = 'visible'
+}, 2000))
+
+    
+
+
 const menu = document.querySelector('.burger')
       list = document.querySelector('.header__list')
       body =document.querySelector('body')
